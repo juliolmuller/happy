acfd53191deaa7aaec8007a7596f429f08e5560d
Julio Muller <juliodolago@gmail.com>
2020-10-14 02:00:23 -0300

    Added and configured ORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import './database/connection'
 
 const port = process.env.PORT || 3030
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (_req, res) => {
   res.send('<h1>Hello, there!</h1>')
