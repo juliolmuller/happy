d69d4ee3557c04e9a499df4a7e2834757461745e
Julio Muller <juliodolago@gmail.com>
2020-10-14 21:47:51 -0300

    Created migrations

---
+++ server/src/database/migrations/1602719974861-create_table_orphanages.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createTableOrphanages1602719974861 implements MigrationInterface {
+
+  private readonly tableName = 'orphanages'
+
+  public async up(query: QueryRunner): Promise<void> {
+    await query.createTable(new Table({
+      name: this.tableName,
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment',
+        },
+        {
+          name: 'name',
+          type: 'varchar',
+        },
+        {
+          name: 'latitude',
+          type: 'decimal',
+          precision: 2,
+          scale: 10,
+        },
+        {
+          name: 'longitude',
+          type: 'decimal',
+          precision: 2,
+          scale: 10,
+        },
+        {
+          name: 'about',
+          type: 'text',
+        },
+        {
+          name: 'instructions',
+          type: 'text',
+        },
+        {
+          name: 'opening_hours',
+          type: 'varchar',
+        },
+        {
+          name: 'open_on_weekends',
+          type: 'boolean',
+          default: false,
+        },
+      ],
+    }))
+  }
+
+  public async down(query: QueryRunner): Promise<void> {
+    await query.dropTable(this.tableName)
+  }
+
+}
