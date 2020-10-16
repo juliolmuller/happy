316864c0b71c09615624e90179593d78f63ee239
Julio Muller <juliodolago@gmail.com>
2020-10-15 23:06:02 -0300

    Added CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import path from 'path'
+import cors from 'cors'
 import express from 'express'
 import 'express-async-errors'
 import routes from './routes'
@@ -8,6 +9,7 @@ import './database/connection'
 const port = process.env.PORT || 3030
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/storage', express.static(path.join(__dirname, '..', 'storage')))
