53b930c8303f1af42a3223c63ec1befe9970d372
Julio Muller <juliodolago@gmail.com>
2020-10-17 23:19:13 -0300

    COnfigured file input and connected to API

--- web/src/pages/OrphanageForm.tsx
+++ web/src/pages/OrphanageForm.tsx
@@ -1,51 +1,62 @@
-import React, { FormEvent, ReactElement, useState } from 'react'
+import React, { ChangeEvent, FormEvent, ReactElement, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import { LeafletMouseEvent } from 'leaflet'
 import { Map, Marker, TileLayer } from 'react-leaflet'
 import { FiPlus } from 'react-icons/fi'
 import mapIcon from '../components/utils/mapIcon'
 import NavBar from '../components/NavBar'
+import api from '../services/api'
 import '../styles/pages/orphanage-form.css'
 
 function OrphanageForm(): ReactElement {
+  const history = useHistory()
+
   const [latitude, setLatitude] = useState(0)
   const [longitude, setLongitude] = useState(0)
-  const [name, setName] = useState('')
-  const [about, setAbout] = useState('')
-  const [instructions, setInstructions] = useState('')
-  const [opening_hours, setOpeningHours] = useState('')
-  const [open_on_weekends, setOpenOnWeekends] = useState(false)
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault()
-    console.log({
-      name,
-      latitude,
-      longitude,
-      about,
-      instructions,
-      opening_hours,
-      open_on_weekends,
-    })
-  }
+  const [openOnWeekends, setOpenOnWeekends] = useState(false)
+  const [photosPreview, setPhotosPreview] = useState<string[]>([])
 
   const handleMapCLick = (event: LeafletMouseEvent) => {
-    const { lat, lng } = event.latlng
+    setLatitude(event.latlng.lat)
+    setLongitude(event.latlng.lng)
+  }
 
-    setLatitude(lat)
-    setLongitude(lng)
+  const handleSelectPhotos = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedPhotos = Array.from(event.target.files || [])
+    setPhotosPreview(selectedPhotos.map(URL.createObjectURL))
+  }
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target as HTMLFormElement)
+    latitude && formData.append('latitude', String(latitude))
+    longitude && formData.append('longitude', String(longitude))
+    formData.append('open_on_weekends', String(openOnWeekends))
+
+    try {
+      const response = await api.post('/orphanages', formData)
+      const orphanageId = response.data.id
+
+      alert('Cadastro realizado com sucesso!')
+      history.push(`/orphanages/${orphanageId}`)
+    } catch (error) {
+      // TODO: display validation errors
+      console.error(error, { ...error })
+    }
   }
 
   return (
-    <div id="page-create-orphanage">
+    <div id="page-orphanage-form">
       <NavBar />
 
       <main>
-        <form onSubmit={handleSubmit} className="create-orphanage-form">
+        <form onSubmit={handleSubmit}>
           <fieldset>
             <legend>Dados</legend>
 
             <Map
-              center={[-25.4719491, -49.2877479]}
+              center={[-25.4321773, -49.2884007]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
               onclick={handleMapCLick}
@@ -59,30 +70,32 @@ function OrphanageForm(): ReactElement {
 
             <div className="input-block">
               <label htmlFor="name">Nome</label>
-              <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-              />
+              <input type="text" id="name" name="name" />
             </div>
 
             <div className="input-block">
               <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea
-                id="name"
-                maxLength={300}
-                value={about}
-                onChange={(event) => setAbout(event.target.value)}
-              />
+              <textarea id="about" name="about" maxLength={300} />
             </div>
 
             <div className="input-block">
-              <label htmlFor="images">Fotos</label>
-              <div className="uploaded-image"></div>
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
-              </button>
+              <label htmlFor="photos">Fotos</label>
+              <input
+                type="file"
+                accept="image/*"
+                id="photo[]"
+                name="photos"
+                multiple
+                onChange={handleSelectPhotos}
+              />
+              <div className="photos-container">
+                {photosPreview.map((photoURL, index) => (
+                  <img key={index} src={photoURL} alt="Foto do orfanato" />
+                ))}
+                <label htmlFor="photo[]" className="new-photo">
+                  <FiPlus size={24} color="#15b6d6" />
+                </label>
+              </div>
             </div>
           </fieldset>
 
@@ -91,35 +104,25 @@ function OrphanageForm(): ReactElement {
 
             <div className="input-block">
               <label htmlFor="instructions">Instruções</label>
-              <textarea
-                id="instructions"
-                value={instructions}
-                onChange={(event) => setInstructions(event.target.value)}
-              />
+              <textarea id="instructions" name="instructions" />
             </div>
 
             <div className="input-block">
               <label htmlFor="opening_hours">Horário de funcionamento</label>
-              <input
-                type="text"
-                id="opening_hours"
-                value={opening_hours}
-                onChange={(event) => setOpeningHours(event.target.value)}
-              />
+              <input type="text" id="opening_hours" name="opening_hours" />
             </div>
 
             <div className="input-block">
               <label htmlFor="open_on_weekends">Atende fim de semana</label>
-
               <div className="button-select">
                 <button
                   type="button"
-                  className={open_on_weekends ? 'active' : ''}
+                  className={openOnWeekends ? 'active' : ''}
                   onClick={() => setOpenOnWeekends(true)}
                 >Sim</button>
                 <button
                   type="button"
-                  className={open_on_weekends ? '' : 'active'}
+                  className={openOnWeekends ? '' : 'active'}
                   onClick={() => setOpenOnWeekends(false)}
                 >Não</button>
               </div>
