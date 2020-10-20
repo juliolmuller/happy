1b08c790f2da9197035624135f14bc28e153ea10
Julio Muller <juliodolago@gmail.com>
2020-10-20 01:03:07 -0300

    Created screens to enter data for orphanages

--- mobile/src/pages/OrphanageCreation/OrphanageForm.tsx
+++ mobile/src/pages/OrphanageCreation/OrphanageForm.tsx
@@ -1,59 +1,92 @@
 import React, { FC, useEffect, useState } from 'react'
 import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity } from 'react-native'
+import { useRoute } from '@react-navigation/native'
 import { Feather } from '@expo/vector-icons'
 import { RectButton } from 'react-native-gesture-handler'
 
-const OrphanageForm: FC = () => (
-  <ScrollView style={styles.container} contentContainerStyle={{ padding: 24 }}>
-    <Text style={styles.title}>Dados</Text>
+interface OrphanageFormRouteParams {
+  latitude: number
+  longitude: number
+}
 
-    <Text style={styles.label}>Nome</Text>
-    <TextInput
-      style={styles.input}
-    />
+const OrphanageForm: FC = () => {
+  const { latitude, longitude } = useRoute().params as OrphanageFormRouteParams
 
-    <Text style={styles.label}>Sobre</Text>
-    <TextInput
-      style={[styles.input, { height: 110 }]}
-      multiline
-    />
+  const [name, setName] = useState('')
+  const [about, setAbout] = useState('')
+  const [instructions, setInstructions] = useState('')
+  const [opening_hours, setOpeningHours] = useState('')
+  const [open_on_weekends, setOpenOnWeekends] = useState(false)
 
-    <Text style={styles.label}>Whatsapp</Text>
-    <TextInput
-      style={styles.input}
-    />
+  const handleFormSubmit = () => {
+    console.log({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    })
+  }
 
-    <Text style={styles.label}>Fotos</Text>
-    <TouchableOpacity style={styles.imagesInput} onPress={() => {}}>
-      <Feather name="plus" size={24} color="#15B6D6" />
-    </TouchableOpacity>
+  return (
+    <ScrollView style={styles.container} contentContainerStyle={{ padding: 24 }}>
+      <Text style={styles.title}>Dados</Text>
 
-    <Text style={styles.title}>Visitação</Text>
-
-    <Text style={styles.label}>Instruções</Text>
-    <TextInput
-      style={[styles.input, { height: 110 }]}
-      multiline
-    />
-
-    <Text style={styles.label}>Horario de visitas</Text>
-    <TextInput
-      style={styles.input}
-    />
-
-    <View style={styles.switchContainer}>
-      <Text style={styles.label}>Atende final de semana?</Text>
-      <Switch
-        thumbColor="#fff"
-        trackColor={{ false: '#ccc', true: '#39CC83' }}
+      <Text style={styles.label}>Nome</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
       />
-    </View>
 
-    <RectButton style={styles.nextButton} onPress={() => {}}>
-      <Text style={styles.nextButtonText}>Cadastrar</Text>
-    </RectButton>
-  </ScrollView>
-)
+      <Text style={styles.label}>Sobre</Text>
+      <TextInput
+        style={[styles.input, { height: 110 }]}
+        multiline
+        value={about}
+        onChangeText={setAbout}
+      />
+
+      <Text style={styles.label}>Fotos</Text>
+      <TouchableOpacity style={styles.imagesInput} onPress={() => {}}>
+        <Feather name="plus" size={24} color="#15B6D6" />
+      </TouchableOpacity>
+
+      <Text style={styles.title}>Visitação</Text>
+
+      <Text style={styles.label}>Instruções</Text>
+      <TextInput
+        style={[styles.input, { height: 110 }]}
+        multiline
+        value={instructions}
+        onChangeText={setInstructions}
+      />
+
+      <Text style={styles.label}>Horario de visitas</Text>
+      <TextInput
+        style={styles.input}
+        value={opening_hours}
+        onChangeText={setOpeningHours}
+      />
+
+      <View style={styles.switchContainer}>
+        <Text style={styles.label}>Atende final de semana?</Text>
+        <Switch
+          thumbColor="#fff"
+          trackColor={{ false: '#ccc', true: '#39CC83' }}
+          value={open_on_weekends}
+          onValueChange={setOpenOnWeekends}
+        />
+      </View>
+
+      <RectButton style={styles.nextButton} onPress={handleFormSubmit}>
+        <Text style={styles.nextButtonText}>Cadastrar</Text>
+      </RectButton>
+    </ScrollView>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
--- mobile/src/pages/OrphanageCreation/OrphanagePosition.tsx
+++ mobile/src/pages/OrphanageCreation/OrphanagePosition.tsx
@@ -1,23 +1,72 @@
-import React, { FC } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { FC, useEffect, useState } from 'react'
+import { View, StyleSheet, Dimensions, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { RectButton } from 'react-native-gesture-handler'
+import MapView, { MapEvent, Marker } from 'react-native-maps'
+import mapMarker from '../../images/map-marker.png'
 
-const OrphanagePosition: FC = () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>Hello, OrphanagePosition!</Text>
-  </View>
-)
+const OrphanagePosition: FC = () => {
+  const { navigate } = useNavigation()
+
+  const [position, setPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+  })
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapStyle}
+        initialRegion={{
+          latitude: -25.4321773,
+          longitude: -49.2884007,
+          latitudeDelta: 0.07,
+          longitudeDelta: 0.07,
+        }}
+        onPress={({ nativeEvent }) => setPosition(nativeEvent.coordinate)}
+      >
+        {position.latitude !== 0 && position.longitude !== 0 && (
+          <Marker icon={mapMarker} coordinate={position} />
+        )}
+      </MapView>
+
+      {position.latitude !== 0 && position.longitude !== 0 && (
+        <RectButton style={styles.nextButton} onPress={() => navigate('OrphanageForm', position)}>
+          <Text style={styles.nextButtonText}>Próximo</Text>
+        </RectButton>
+      )}
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    backgroundColor: '#2ab5d1',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
+    position: 'relative',
   },
 
-  text: {
-    color: '#fff',
-    fontSize: 20,
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  nextButton: {
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 56,
+
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 40,
+  },
+
+  nextButtonText: {
+    fontFamily: 'Nunito800',
+    fontSize: 16,
+    color: '#FFF',
   },
 })
 
