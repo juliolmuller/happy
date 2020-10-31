76d62d3fd836e5f744bfab05124b4117c5c0df8f
Julio Muller <juliodolago@gmail.com>
2020-10-31 02:30:12 -0300

    Fixed file name generation

--- server/src/uploads.ts
+++ server/src/uploads.ts
@@ -1,12 +1,15 @@
 import multer from 'multer'
 import path from 'path'
+import crypto from 'crypto'
 
 const storage = multer.diskStorage({
   destination: path.join(__dirname, '..', 'storage'),
   filename(req, file, callback) {
     const timestamp = Date.now()
-    const safeFileName = escape(file.originalname)
-    const uniqueFileName = `${timestamp}-${safeFileName}`
+    const originalName = file.originalname
+    const safeFileName = crypto.createHash('md5').update(originalName).digest('hex')
+    const fileExtension = originalName.slice(originalName.lastIndexOf('.'))
+    const uniqueFileName = `${timestamp}-${safeFileName}${fileExtension}`
 
     callback(null, uniqueFileName)
   },
