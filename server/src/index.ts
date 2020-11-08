74ee69215549b22815cf0d4ab51e3feb093f7f84
Julio Muller <juliodolago@gmail.com>
2020-11-08 16:05:06 -0300

    Added 'morgan' to log requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import morgan from 'morgan'
 import routes from './routes'
 import errorHandler from './errors/handler'
 import './database/connection'
@@ -16,6 +17,7 @@ if (config.error && process.env.NODE_ENV !== 'production') {
 const app = express()
 
 app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(routes)
 app.use('/photos', express.static(path.join(__dirname, '..', 'storage')))
