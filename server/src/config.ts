a87c09a1f80f4886ee11694f549a89b24451344d
Julio Muller <juliodolago@gmail.com>
2020-12-28 18:21:30 -0300

    Updated "dotenv" to run only in non-production environment

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv-flow'
 
-const config = dotenv.config({ purge_dotenv: true })
+if (process.env.NODE_ENV !== 'production') {
+  const config = dotenv.config({ purge_dotenv: true })
 
-if (config.error && process.env.NODE_ENV !== 'production') {
-  throw config.error
+  if (config.error && process.env.NODE_ENV !== 'production') {
+    throw config.error
+  }
 }
