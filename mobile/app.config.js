62c0e5720ff72c89fe2258c7ab8135683d8a87a8
Julio Muller <juliodolago@gmail.com>
2020-12-28 17:26:19 -0300

    Updated Expo configuration file

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv-flow'
+import npmConfig from './package.json'
 
 const config = dotenv.config({ purge_dotenv: true })
 
@@ -9,28 +10,35 @@ if (config.error) {
 export default {
   expo: {
     name: 'Happy',
-    version: '1.1.0',
+    version: npmConfig.version,
+    extra: {
+      ...config.parsed,
+    },
     orientation: 'portrait',
+    platforms: ['android', 'ios'],
     icon: './src/images/icon.png',
     splash: {
       backgroundColor: '#15c3d6',
       image: './src/images/splash.png',
       resizeMode: 'cover',
     },
-    updates: {
-      fallbackToCacheTimeout: 0,
-    },
     assetBundlePatterns: [
       '**/*',
     ],
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    android: {
+      package: 'juliolmuller.happy',
+      versionCode: 1,
+    },
     ios: {
       supportsTablet: true,
+      bundleIdentifier: 'juliolmuller.happy',
+      buildNumber: '1.0.0',
     },
     web: {
       favicon: './src/images/favicon.png',
-    },
-    extra: {
-      API_URL: process.env.API_URL,
     },
   },
 }
