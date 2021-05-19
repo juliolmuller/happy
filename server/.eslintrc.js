05aa6f5f80f83ee3c8d287417cd164922fd7d2ee
Julio Muller <juliodolago@gmail.com>
2021-05-18 23:27:39 -0300

    Changed linting preset

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -5,24 +5,21 @@ module.exports = {
     es6: true,
   },
   extends: [
-    '@lacussoft',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+    'lacussoft',
+    'lacussoft/typescript',
   ],
   ignorePatterns: [
     'build/',
   ],
-  plugins: [
-    '@typescript-eslint',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
-  },
   rules: {
-    'camelcase': ['error', { allow: ['purge_dotenv', 'opening_hours', 'open_on_weekends', 'orphanage_id'] }],
-    'arrow-body-style': 'off',
+    'camelcase': ['error', {
+      allow: [
+        'purge_dotenv',
+        'opening_hours',
+        'open_on_weekends',
+        'orphanage_id',
+      ],
+    }],
     'no-console': 'off',
   },
 }
