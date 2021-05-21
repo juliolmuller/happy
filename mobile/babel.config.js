f73d4829a5a8bf0a8f14cd8af9938e28703531b7
Julio Muller <juliodolago@gmail.com>
2021-05-21 00:29:19 -0300

    Updated Babel configurations

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  presets: ['babel-preset-expo'],
+/* eslint-env node */
+module.exports = (api) => {
+  api.cache(true)
+
+  return {
+    presets: [
+      'babel-preset-expo',
+    ],
+    plugins: [
+      ['module-resolver', {
+        alias: {
+          '@': './src',
+        },
+      }],
+    ],
+  }
 }
