6b2e106a44d0cec1c274fa553486c33f0d285502
juliolmuller <juliolmuller@outlook.com>
2026-06-06 23:21:17 -0300

    feat(mobile): upgrade project to Expo SDK v56

---
+++ mobile/app.config.ts
@@ -0,0 +1,60 @@
+/* eslint-disable @eslint-community/eslint-comments/disable-enable-pair, camelcase */
+import dotenv from 'dotenv-flow'
+import { type ExpoConfig } from 'expo/config';
+import npmConfig from './package.json' with { type: 'json' }
+
+const config = dotenv.config({ purge_dotenv: true })
+
+if (config.error) {
+  throw config.error
+}
+
+export default {
+  name: 'Happy',
+  slug: 'happy',
+  scheme: 'happy',
+  version: npmConfig.version,
+  owner: 'juliolmuller',
+  orientation: 'portrait',
+  icon: './src/assets/img/favicon.png',
+  userInterfaceStyle: 'light',
+  assetBundlePatterns: ['**/*'],
+  plugins: [
+    'expo-font',
+    [
+      'expo-splash-screen',
+      {
+        backgroundColor: '#15c3d6',
+        image: './src/assets/img/splash.png',
+        resizeMode: 'contain',
+        enableFullScreenImage_legacy: true,
+      },
+    ],
+    [
+      'expo-build-properties',
+      {
+        android: {
+          usesCleartextTraffic: true,
+        },
+      },
+    ],
+  ],
+  extra: {
+    ...config.parsed,
+    eas: {},
+  },
+  android: {
+    package: 'com.juliolmuller.happy',
+    adaptiveIcon: {
+      foregroundImage: './src/assets/img/favicon.png',
+      backgroundColor: '#ffffff',
+    },
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'juliolmuller.happy',
+  },
+  web: {
+    favicon: './src/assets/img/favicon.png',
+  },
+} satisfies ExpoConfig;
